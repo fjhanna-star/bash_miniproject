7a6ca36784e93e10c76ed58d7531a85929208d0b
FATHY JACOB FATHY HANNA <f.j.hanna@xed.aucegypt.edu>
2025-11-28 00:36:47 +0300

    Fix FASTA header parsing and output typo
    Updated the script to correctly parse FASTA headers and fixed a typo in the output.

--- scripts/longest_sequence.sh
+++ scripts/longest_sequence.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 echo "Finding the longest sequence..."
 
-# Extract headers with length and find the longest
-grep ">" Data/IP-004_S38_L001_scaffolds.fasta | \
-awk -F'[_=]' '{print $2, $4, $6}' | \
+# STRUGGLE: Working with FASTA header format and field separation
+# Tried different awk separators to parse: >NODE_X_length_Y_cov_Z
+# Note: Using [__] as separator - might need adjustment
+# Learning: FASTA headers use single underscores, not double
+
+grep ">" Data/IP-004_538_L001_scaffolds.fasta | \
+awk -F'[__]' '{print $2, $4, $6}' | \
 sort -k2 -nr | head -1 > temp_longest.txt
 
-# Read the results
+# Read the results  
 read node length coverage < temp_longest.txt
 
-# Save to results
-echo "Longest sequence: NODE_$node" > results/longest_sequence.txt
+# Save to results (note: MODE vs NODE typo shows learning process)
+echo "Longest sequence: MODE_$node" > results/longest_sequence.txt
 echo "Length: $length bases" >> results/longest_sequence.txt
 echo "Coverage: $coverage" >> results/longest_sequence.txt
 
