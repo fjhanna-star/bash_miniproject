fef9aac2a86376c1c45ee574c975e70b93cb49ef
FATHY JACOB FATHY HANNA <f.j.hanna@xed.aucegypt.edu>
2025-11-28 00:34:28 +0300

    Change FASTA file for sequence counting
    Updated the script to count sequences in a different FASTA file.

--- scripts/count_sequences.sh
+++ scripts/count_sequences.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Counting sequences in FASTA file..."
-count=$(grep -c ">" Data/IP-004_S38_L001_scaffolds.fasta)
+
+# Note: Using ">" instead of "^>" - might count extra characters in sequences
+# Learning: FASTA headers start with > at beginning of line
+count=$(grep -c ">" Data/IP-004_538_L001_scaffolds.fasta)
+
 echo $count > results/sequence_count.txt
 echo "Found $count sequences. Saved to results/sequence_count.txt"
